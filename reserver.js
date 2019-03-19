7e8d27fb69643fba242cc8df1adc330900b4575a
손량 <loop.infinitely@gmail.com>
2019-03-19 10:47:43 +0900

    added brute force

--- reserver.js
+++ reserver.js
@@ -6,9 +6,8 @@ let url = 'http://hi.hana.hs.kr/SYSTEM_Plan/Lib_System';
 url += '/Lib_System_Reservation/reservation_exec.asp';
 
 function generate_data(seat) {
-    let result = [], time_list = [], today = new Date();
-    if (today.getDay() % 6) time_list = [1, 4];
-    else time_list = [7, 9, 10]
+    let result = [], today = new Date();
+    let time_list = [...Array(5).keys()];
     for (time of time_list)
         result.push(`code=001&s_code=${seat}&t_code=${time}`);
     return result;
