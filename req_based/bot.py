a04212e9fbf4c360fda598f17a51633d8fd95866
손량 <loop.infinitely@gmail.com>
2019-03-23 10:32:59 +0900

    fixed a bug

--- req_based/bot.py
+++ req_based/bot.py
@@ -38,14 +38,16 @@ print('Ready to flood the server')
 
 library_url = '/SYSTEM_Plan/Lib_System/Lib_System_Reservation' + \
               '/reservation_exec.asp'
+if datetime.today().weekday() < 5:
+    time_codes = {1, 4}
+else:
+    time_codes = {6, 7, 8, 9, 10}
+success = set()
+
 for seat in range(1, 73):
     print(Fore.CYAN + ('Trying seat #%d' % (seat)) + Style.RESET_ALL)
-    if datetime.today().weekday() < 5:
-        time_codes = {1, 4}
-    else:
-        time_codes = {6, 7, 8, 9, 10}
-    while len(time_codes) > 0:
-        reserve_time = min(time_codes)
+    time_codes = time_codes - success
+    for reserve_time in time_codes:
         print('  for time_code %d... ' % (reserve_time), end='')
         data = {'code': '001', 's_code': seat, 't_code': reserve_time}
         response = sess.post(baseurl + library_url, data=data)
@@ -53,7 +55,7 @@ for seat in range(1, 73):
               (response.text[:55] and '..') + ' ', end='')
         if '되었습니다' in response.text and '비정상' not in response.text:
             print(Fore.GREEN + 'OK' + Style.RESET_ALL)
-            time_codes.remove(reserve_time)
+            success.add(time)
         elif '비정상' in response.text or '로그아웃' in response.text:
             print(Fore.YELLOW + 'RETRY' + Style.RESET_ALL)
             login(second=True)
