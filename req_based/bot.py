037f6e190f107b11c782ff9b782180da1cf0a938
손량 <loop.infinitely@gmail.com>
2019-03-26 14:10:49 +0900

    changed algorithm

--- req_based/bot.py
+++ req_based/bot.py
@@ -42,22 +42,24 @@ if datetime.today().weekday() < 5:
     time_codes = {1, 4}
 else:
     time_codes = {6, 7, 8, 9, 10}
-success = set()
 
-for seat in range(1, 73):
-    print(Fore.CYAN + ('Trying seat #%d' % (seat)) + Style.RESET_ALL)
-    time_codes = time_codes - success
+try:
     for reserve_time in time_codes:
-        print('  for time_code %d... ' % (reserve_time), end='')
-        data = {'code': '001', 's_code': seat, 't_code': reserve_time}
-        response = sess.post(baseurl + library_url, data=data)
-        print(response.text[:55] + \
-              (response.text[:55] and '..') + ' ', end='')
-        if '되었습니다' in response.text and '비정상' not in response.text:
-            print(Fore.GREEN + 'OK' + Style.RESET_ALL)
-            success.add(time)
-        elif '비정상' in response.text or '로그아웃' in response.text:
-            print(Fore.YELLOW + 'RETRY' + Style.RESET_ALL)
-            login(second=True)
-        else:
-            print(Fore.RED + 'FAIL' + Style.RESET_ALL)
+        print(Fore.CYAN + \
+              ('Trying time code %d' % (reserve_time)) + Style.RESET_ALL)
+        for seat in range(1, 73):
+            print('  for seat #%d... ' % (seat), end='')
+            data = {'code': '001', 's_code': seat, 't_code': reserve_time}
+            response = sess.post(baseurl + library_url, data=data)
+            print(response.text[:55] + \
+                  (response.text[:55] and '..') + ' ', end='')
+            if '되었습니다' in response.text and '비정상' not in response.text:
+                print(Fore.GREEN + 'OK' + Style.RESET_ALL)
+                break
+            elif '비정상' in response.text or '로그아웃' in response.text:
+                print(Fore.YELLOW + 'RETRY' + Style.RESET_ALL)
+                login(second=True)
+            else:
+                print(Fore.RED + 'FAIL' + Style.RESET_ALL)
+except InterruptedError:
+    print(Fore.YELLOW + 'Aborting the bot' + Style.RESET_ALL)
